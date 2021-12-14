716fb310a1c82c6eb0e49b8376b4315c83de28c2
HofmannAlexandre <alexandre.hofmannwild@gmail.com>
2021-12-14 17:01:02 +0100

    prequest20

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -12,9 +12,9 @@ class DefaultController extends AbstractController
      * @Route("/", name="app_index")
      */
     public function index(): Response
-{
-    return $this->render('program/index.html.twig', [
-       'website' => '',
-    ]);
-}
+    {
+        return $this->render('program/index.html.twig', [
+        'website' => '',
+        ]);
+    }
 }
